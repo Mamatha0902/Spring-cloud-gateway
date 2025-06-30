95671575ac03754622cc8c959f1653704f6de37e
vedhasri <vedhasrivallakatla@gmail.com>
2025-06-30 13:31:09 +0530

    added the  dynamically cors filters in gateway

--- src/main/java/com/example/gateway_application/configuration/GatewayConfig.java
+++ src/main/java/com/example/gateway_application/configuration/GatewayConfig.java
@@ -3,6 +3,7 @@ package com.example.gateway_application.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.web.cors.reactive.CorsUtils;
@@ -13,21 +14,45 @@ import org.springframework.web.server.WebFilterChain;
 
 @Configuration
 public class GatewayConfig {
+    private static final String MAX_AGE = "3600";
 
     @Bean
     public WebFilter corsFilter() {
-        return (ServerWebExchange ctx, WebFilterChain chain) -> {
-            ServerHttpRequest request = ctx.getRequest();
+        return (ServerWebExchange exchange, WebFilterChain chain) -> {
+            ServerHttpRequest request = exchange.getRequest();
+
             if (CorsUtils.isCorsRequest(request)) {
-                ServerHttpResponse response = ctx.getResponse();
-                HttpHeaders headers = response.getHeaders();
+                HttpHeaders headers = exchange.getResponse().getHeaders();
                 String origin = request.getHeaders().getOrigin();
-                headers.add("Access-Control-Allow-Origin", origin);
-                headers.add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-                headers.add("Access-Control-Allow-Headers", "*");
-                headers.add("Access-Control-Allow-Credentials", "true");
+
+                if (origin != null && !headers.containsKey(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN)) {
+                    headers.set(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, origin);
+                }
+
+                if (!headers.containsKey(HttpHeaders.ACCESS_CONTROL_ALLOW_METHODS)) {
+                    headers.set(HttpHeaders.ACCESS_CONTROL_ALLOW_METHODS, "GET, POST, PUT, DELETE, OPTIONS");
+                }
+
+                if (!headers.containsKey(HttpHeaders.ACCESS_CONTROL_ALLOW_HEADERS)) {
+                    headers.set(HttpHeaders.ACCESS_CONTROL_ALLOW_HEADERS, "Content-Type, Authorization, *");
+                }
+
+                if (!headers.containsKey(HttpHeaders.ACCESS_CONTROL_ALLOW_CREDENTIALS)) {
+                    headers.set(HttpHeaders.ACCESS_CONTROL_ALLOW_CREDENTIALS, "true");
+                }
+
+                if (!headers.containsKey(HttpHeaders.ACCESS_CONTROL_MAX_AGE)) {
+                    headers.set(HttpHeaders.ACCESS_CONTROL_MAX_AGE, MAX_AGE);
+                }
+
+                // Short-circuit OPTIONS requests
+                if (request.getMethod() == HttpMethod.OPTIONS) {
+                    exchange.getResponse().setStatusCode(org.springframework.http.HttpStatus.OK);
+                    return exchange.getResponse().setComplete();
+                }
             }
-            return chain.filter(ctx);
+
+            return chain.filter(exchange);
         };
     }
 }
